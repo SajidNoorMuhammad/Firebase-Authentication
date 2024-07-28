63cb95a6dca048a145451303f2dee3d90f8b0c60
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-07-28 16:41:56 +0500

    Firebase

---
+++ app.js
@@ -0,0 +1,83 @@
+import {
+    auth,
+    createUserWithEmailAndPassword,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signOut,
+    sendEmailVerification,
+    sendPasswordResetEmail 
+} from "./firebase.js"
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        console.log(user)
+    } else {
+        console.log("user not exist")
+    }
+});
+
+let signup = () => {
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+    createUserWithEmailAndPassword(auth, email.value, password.value)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log("user", user)
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(error)
+        });
+
+    email.value = "";
+    password.value = "";
+}
+
+let signupBtn = document.getElementById("signupBtn")
+signupBtn.addEventListener('click', signup)
+
+let login = () => {
+    let lemail = document.getElementById("lemail");
+    let lpassword = document.getElementById("lpassword");
+    signInWithEmailAndPassword(auth, lemail.value, lpassword.value)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            location.assign("https://sajidnoormuhammad.github.io/Quiz-App/")
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(error)
+        });
+    lemail.value = "";
+    lpassword.value = "";
+}
+
+let loginBtn = document.getElementById("loginBtn")
+loginBtn.addEventListener('click', login)
+
+
+let logout = () => {
+
+    signOut(auth)
+        .then(() => {
+            console.log("Logout Successfully")
+        }).catch((error) => {
+            // An error happened.
+        });
+}
+
+let logoutBtn = document.getElementById("logoutBtn")
+logoutBtn.addEventListener('click', logout)
+
+let verifyBtn = document.getElementById("verifyBtn")
+verifyBtn.addEventListener('click', () => {
+    sendEmailVerification(auth.currentUser)
+        .then(() => {
+            console.log("sent")
+        });
+})
+
+
+
---
+++ firebase.js
@@ -0,0 +1,31 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signOut,
+    sendEmailVerification,
+    sendPasswordResetEmail 
+} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyByBkrY1_haXPuligYvo-JLuQvJv_m2ONg",
+    authDomain: "quiz-app-25040.firebaseapp.com",
+    projectId: "quiz-app-25040",
+    storageBucket: "quiz-app-25040.appspot.com",
+    messagingSenderId: "1075536468084",
+    appId: "1:1075536468084:web:ce04ebe5ff5cc69df19921"
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export {
+    auth,
+    createUserWithEmailAndPassword,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signOut,
+    sendEmailVerification,
+    sendPasswordResetEmail 
+}
